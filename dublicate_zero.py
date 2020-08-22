3adf13ab48429bacd9c53cbb5df6a91807797677
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-08-22 15:53:48 +0530

    Day 2

---
+++ dublicate_zero.py
@@ -0,0 +1,28 @@
+# import timeit
+
+
+# def dublicate_zero(arr):
+#     code_to_test="""
+#     for i in range(len(arr)-1,-1,-1):
+#         if arr[i]==0:
+#             arr.pop()
+#             arr.insert(i,0)                      
+#     """
+#     elapsed_time = timeit.timeit(code_to_test, number=100)/100
+#     print(elapsed_time)
+#     return arr        
+ 
+# a=[1,0,2,3,0,4,5,0]
+# print(dublicate_zero(a)) 
+
+import timeit
+arr=[1,0,2,3,0,4,5,0]
+code_to_test="""
+for i in range(len(arr)-1,-1,-1):
+    if arr[i]==0:
+    arr.pop()
+    arr.insert(i,0)                      
+"""
+elapsed_time = timeit.timeit(code_to_test, number=100)/100
+print(elapsed_time)
+print(arr) 
