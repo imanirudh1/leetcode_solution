0ed6027dd59e12fb04f3a95c6348926744e49c7c
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-09-22 12:41:29 +0530

    String Comparision

--- string_compression.py
+++ string_compression.py
@@ -1,35 +1,40 @@
-chars=["a","a","b","b","c","c","c"]
-# def compress(chars):
-#         counter=1
-#         i=1
-#         r=''
-#         while i<len(chars):
-#             if chars[i]==chars[i-1]:
-#                 counter+=1
-#             else:
-#                 r=r+chars[i-1]+str(counter)
-#                 counter=1
-#             i+=1
-#         r=r+chars[i-1]+str(counter)
-#         chars=list(r)
-#         return chars
-
-
+chars="aabc"
 def compress(chars):
-    i, j = 0, 0
-    while j < len(chars):
-        cur = chars[j]
-        cnt = 0
-        while j < len(chars) and chars[j] == cur:
-            j += 1
-            cnt += 1
-        chars[i] = cur
-        i += 1
-        if cnt > 1:
-            cnt_str = list(str(cnt))
-            for s in cnt_str:
-                chars[i] = s
-                i += 1
-    return i
+        counter=1
+        i=1
+        r=''
+        while i<len(chars):
+            if chars[i]==chars[i-1]:
+                counter+=1
+            else:
+                if counter > 1:
+                    r=r+chars[i-1]+str(counter)
+                    counter=1
+                else:
+                    r=r+chars[i-1]
+                    counter=1    
+            i+=1
+        if counter > 1:    
+            r=r+chars[i-1]+str(counter)
+        else:
+            r=r+chars[i-1]
+        return r
 
 print(compress(chars))        
+# def compress(chars):
+#     i, j = 0, 0
+#     while j < len(chars):
+#         cur = chars[j]
+#         cnt = 0
+#         while j < len(chars) and chars[j] == cur:
+#             j += 1
+#             cnt += 1
+#         chars[i] = cur
+#         i += 1
+#         if cnt > 1:
+#             cnt_str = list(str(cnt))
+#             for s in cnt_str:
+#                 chars[i] = s
+#                 i += 1
+#     return i
+
